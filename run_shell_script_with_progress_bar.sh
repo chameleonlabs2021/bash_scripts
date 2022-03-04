bf9c6ea463ad70bbc22ffbe7fcac5539909f7a60
chameleonlabs2021 <96870965+chameleonlabs2021@users.noreply.github.com>
2022-03-04 23:58:17 +0800

    Add files via upload

---
+++ run_shell_script_with_progress_bar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+`nohup sh /home/rahul/test.sh  >/tmp/test.out 2>&1 &`
+while true; do
+        cnt=`ps -ef|grep test.sh|awk '{print $2}'|wc -l`
+        if [ $cnt -gt 1 ]; then
+                echo "proc check" `ps -ef|grep test.sh`
+            # Frame #1
+                printf "\r< Loading..."
+                    sleep 0.5
+                        # Frame #2
+                            printf "\r> Loading..."
+                                sleep 0.5
+                        else
+                                exit 1
+        fi
+                        done
